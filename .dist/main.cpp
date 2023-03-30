e14538d0796a8634a01be4c7fbae383bd730fb4b
Bokkieboy <gamecastbokkie@gmail.com>
2023-03-30 23:54:45 +0100

    Created Menu
    Can see option for all different calculations

---
+++ .dist/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+void displaymenu(){
+    using namespace std;
+    cout<<"===================================================== \n";
+    cout<<" \t\tTime Calc \t \n ";
+    cout<<"===================================================== \n";
+    cout<<" 1.Convert seconds to hours\n";
+    cout<<" 2.Convert seconds to minutes\n";
+    cout<<" 3.Convert minutes to hours \n";
+    cout<<" 4.Convert hours to seconds \n";
+    cout<<" 5.Convert minutes to seconds \n";
+    cout<<" 6.Exit \n";
+}
+
+
+int main(){
+    displaymenu();
+    return 0;
+}
